064348f4865ca41a9c9a725b9cb3a75066ef7698
Leonid Dinershtein <leonid@dinershtein.com>
2020-01-26 22:06:30 +0300

    Scout70

---
+++ data.js
@@ -0,0 +1,836 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-scouthouse-70",
+      "name": "Scouthouse 70",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -3.1178331890164106,
+        "pitch": -0.1535519310670761,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.974883286197267,
+          "pitch": 0.2227125506849177,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Веранда",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.9987406966717955,
+        "pitch": 0.09749364258330928,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6128542998768616,
+          "pitch": 0.5037059431862154,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 2.3946068649063967,
+          "pitch": 0.35336692301563843,
+          "rotation": 4.71238898038469,
+          "target": "2--ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--ii",
+      "name": "Веранда II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.569267261052948,
+        "pitch": 0.14769946140184942,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9558813890271924,
+          "pitch": 0.1954636189209289,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.5663838950074869,
+          "pitch": 0.5515165500698522,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Тераса",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.204427971023767,
+        "pitch": 0.040462100269333945,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7904274236816757,
+          "pitch": 0.41442045035043407,
+          "rotation": 0,
+          "target": "4--ii"
+        },
+        {
+          "yaw": 0.24548888248080303,
+          "pitch": 0.43852366962376266,
+          "rotation": 0,
+          "target": "2--ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--ii",
+      "name": "Тераса II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.397362637709218,
+        "pitch": 0.010975985273786293,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0303564552279774,
+          "pitch": 0.28039525319360337,
+          "rotation": 0,
+          "target": "10---iii"
+        },
+        {
+          "yaw": -2.6471383824718107,
+          "pitch": 0.1917401627355506,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.3300215252322438,
+          "pitch": 0.8319768499999913,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "Скалодром",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 3.0218255929606617,
+        "pitch": 0.01930616385616979,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4412497024594657,
+          "pitch": 0.17507469521042296,
+          "rotation": 0,
+          "target": "10---iii"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3010675442216924,
+          "pitch": 0.0493810268705861,
+          "title": "Скалодром (опция)",
+          "text": "Когда мы в первый раз подошли к фронтонам и оценили их высоту, «скалолазная стенка» — это первая аналогия, пришедшая в голову. Попробовали, сделали, всем понравилось. Благодаря модульной системе крепления зацепов, можно регулировать расстояние и сложность трасс. В комплект входит 60 зацепов, точка страховки, специальная усиленная стена."
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "Прихожая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.9092984668622908,
+        "pitch": 0.24198333158719265,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8387138101128144,
+          "pitch": 0.4715289029418166,
+          "rotation": 0,
+          "target": "8--"
+        },
+        {
+          "yaw": -1.9900410198555818,
+          "pitch": 0.2683569098721321,
+          "rotation": 1.5707963267948966,
+          "target": "7-"
+        },
+        {
+          "yaw": -0.30934987374635625,
+          "pitch": 0.708761390047929,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "Санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.562995826702849,
+        "pitch": 0.2577195404774315,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.73068956326102,
+          "pitch": 0.9339203845184834,
+          "rotation": 3.141592653589793,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8--",
+      "name": "Кухня-гостиная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -3.0875795769647176,
+        "pitch": 0.08380401905305312,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5646643932624116,
+          "pitch": 0.2203204921487938,
+          "rotation": 0,
+          "target": "10---iii"
+        },
+        {
+          "yaw": -2.4520005885196436,
+          "pitch": 0.27564494694525976,
+          "rotation": 0,
+          "target": "9---ii"
+        },
+        {
+          "yaw": 0.010844744594805888,
+          "pitch": 0.5890405232581841,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9---ii",
+      "name": "Кухня-гостиная II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 1.1902376934272798,
+        "pitch": 0.20390209764684641,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.16249395896408458,
+          "pitch": 0.2141372806841062,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 1.4630671194886498,
+          "pitch": 0.3533537400538478,
+          "rotation": 0,
+          "target": "10---iii"
+        },
+        {
+          "yaw": 0.8337010685364135,
+          "pitch": 0.09310414855953475,
+          "rotation": 0,
+          "target": "11-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10---iii",
+      "name": "Кухня-гостиная III",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.9692236372967162,
+        "pitch": 0.4365534589323463,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3704669062880743,
+          "pitch": 0.2855321302177707,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -1.57768750418834,
+          "pitch": 0.25543081263415957,
+          "rotation": 0,
+          "target": "9---ii"
+        },
+        {
+          "yaw": 0.2639965126657646,
+          "pitch": 0.22417366571247754,
+          "rotation": 0,
+          "target": "11-"
+        },
+        {
+          "yaw": -3.0810158674336883,
+          "pitch": 0.5935916528225196,
+          "rotation": 0,
+          "target": "4--ii"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.689943731515977,
+          "pitch": 0.39162071546814303,
+          "title": "Печь / камин",
+          "text": "В стандартной комплектации дома мы сознательно отказались от печи по-умолчанию.<br>Мы предлагаем устанавливать комплект дымохода, несгораемый лист на пол, безопасно проходим через стену и кровлю, растягиваем трубу на кровле. Дальше вы выбираете печь по своему вкусу, оплачиваете и мы устанавливаем ее в ваш дом."
+        }
+      ]
+    },
+    {
+      "id": "11-",
+      "name": "Лестница",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.782460688410362,
+        "pitch": -0.19416872634014126,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3582372731233061,
+          "pitch": -0.14608776833733828,
+          "rotation": 0,
+          "target": "12---"
+        },
+        {
+          "yaw": -0.11548953280363783,
+          "pitch": 0.6925484910559447,
+          "rotation": 0,
+          "target": "10---iii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12---",
+      "name": "Холл второго этажа",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.9711784098453222,
+        "pitch": 0.1763772181175849,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3116031061118001,
+          "pitch": 0.559329582650701,
+          "rotation": 0,
+          "target": "15-"
+        },
+        {
+          "yaw": 1.910246505029093,
+          "pitch": 0.31893399364923525,
+          "rotation": 0,
+          "target": "13----ii"
+        },
+        {
+          "yaw": -3.122885400323341,
+          "pitch": 0.9451245330497784,
+          "rotation": 0,
+          "target": "10---iii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13----ii",
+      "name": "Холл вторго этажа II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -3.102041869481557,
+        "pitch": 0.267139653772567,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0937009569997613,
+          "pitch": 0.28762713072540613,
+          "rotation": 0,
+          "target": "16-"
+        },
+        {
+          "yaw": 1.4893239226757107,
+          "pitch": -0.8922676330239749,
+          "rotation": 0,
+          "target": "14--"
+        },
+        {
+          "yaw": 0.12535094912578515,
+          "pitch": 0.671468648660559,
+          "rotation": 0,
+          "target": "12---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14--",
+      "name": "Чердак-лофт",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 3.078272249864197,
+        "pitch": 0.38432024790512465,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6090404782273477,
+          "pitch": 0.8756426881494903,
+          "rotation": 0,
+          "target": "13----ii"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.512779444000701,
+          "pitch": 0.22165382463049355,
+          "title": "Лофт (опция)",
+          "text": "В детстве каждый мечтает о своем уголке, куда нет входа маме. Или жене. А может и мужу) Где можно играть, просто лежать и смотреть в окно, не обязательно постоянно поддерживать «стерильный» порядок. Где можно работать в тишине и тебя никто не найдет. А еще там можно смотреть фильмы с проектора на противоположной стене, свесив ноги."
+        }
+      ]
+    },
+    {
+      "id": "15-",
+      "name": "Спальня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -3.114974325343665,
+        "pitch": 0.31528701962858996,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.023031620522544,
+          "pitch": 0.954162206656683,
+          "rotation": 3.141592653589793,
+          "target": "12---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-",
+      "name": "Крыша",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.4300176922178345,
+        "pitch": 0.2780765559224392,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8655944183387945,
+          "pitch": 0.4974227921796661,
+          "rotation": 0,
+          "target": "17--ii"
+        },
+        {
+          "yaw": 0.02154107402020955,
+          "pitch": 0.8307188452758041,
+          "rotation": 0,
+          "target": "13----ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17--ii",
+      "name": "Крыша II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.605318971544083,
+        "pitch": 0.3096611191776546,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.480598555441617,
+          "pitch": 0.9697725135905628,
+          "rotation": 3.141592653589793,
+          "target": "16-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
